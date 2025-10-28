42f7151b17cc5716929be534e749968165f6d172
Phalgun Vaddepalli <phalgunv@users.noreply.github.com>
2025-10-28 10:07:17 +0000

    chore: ng update migrations to Angular 12

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,7 +1,7 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone-testing';
-import 'zone.js/dist/zone';
+import 'zone.js/testing';
+import 'zone.js';
 import 'zone.js/dist/sync-test';
 import { getTestBed } from '@angular/core/testing';
 import {
