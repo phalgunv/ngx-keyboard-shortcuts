b60986b18f7c25b22540c25c81cb2c0d3a10b4b5
Phalgun Vaddepalli <phalgunv@users.noreply.github.com>
2025-10-28 12:31:32 +0000

    chore(demo): copy built library into demo and remove core-js polyfills for evergreen browsers

--- src/demo/polyfills.ts
+++ src/demo/polyfills.ts
@@ -18,25 +18,7 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. */
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/weak-map';
-import 'core-js/es/set'; // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es/reflect';
- // Run `npm install --save web-animations-js`.
+/** Polyfills for evergreen browsers (no core-js) */
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
